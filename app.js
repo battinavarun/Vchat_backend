038807962b1fc674cd29690d258ad24534b83a59
BATTINA VARUN <battinavarun@gmail.com>
2025-03-18 15:42:11 +0530

    Updated API URL and fixed CORS issue

--- app.js
+++ app.js
@@ -7,29 +7,29 @@ const { GoogleGenerativeAI } = require("@google/generative-ai");
 const app = express();
 //!Middlewares
 const corsOptions = {
-    origin: ["http://localhost:5174", "http://localhost:5173"],
-  };
-  app.use(cors(corsOptions));
-  
+    origin: ["https://vchat-gemini.vercel.app"],
+};
+app.use(cors(corsOptions));
+
 app.use(express.json());
 // Access your API key as an environment variable (see "Set up your API key" above)
 const genAI = new GoogleGenerativeAI(process.env.API_KEY);
 
 //!Generate content route
 app.post("/generate", async (req, res) => {
-  const { prompt } = req.body;
-  try {
-    // For text-only input, use the gemini-pro model
-    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+    const { prompt } = req.body;
+    try {
+        // For text-only input, use the gemini-pro model
+        const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
-    const result = await model.generateContent(prompt);
-    const response = await result.response;
-    const text = response.text();
-    res.send(text);
-  } catch (error) {
-    console.log(error);
-    res.status(500).send("Failed to generate content");
-  }
+        const result = await model.generateContent(prompt);
+        const response = await result.response;
+        const text = response.text();
+        res.send(text);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send("Failed to generate content");
+    }
 });
 
 //!Start the server
